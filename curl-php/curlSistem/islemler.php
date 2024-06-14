d87368dcdbddb429d201cafaa938b967b41b23be
bahti41 <sonmezbahtiyar4@gmail.com>
2024-06-14 20:52:08 +0300

    json ile istek atma

---
+++ curl-php/curlSistem/islemler.php
@@ -0,0 +1,38 @@
+<?php
+
+$uye = array(
+    "id" => 1,
+    "ad" => "Bahtiyar",
+    "soyadi" => "Sonmez"
+);
+
+switch (@$_GET["islem"]):
+
+
+    case "normal":
+
+        echo json_encode($uye);
+
+        break;
+
+    case "kayitlar":
+
+        echo 'veritabanı kayıtları ve işlemleri olacak';
+
+        break;
+
+    case "oturum":
+
+        echo 'Oturum İşlemleri olacak';
+
+        break;
+
+    case "dosyalar":
+
+        echo 'Dosyalar ile ilgili bilgi olacak';
+
+        break;
+
+
+
+endswitch;
--- curl-php/php-curl/index.php
+++ curl-php/php-curl/index.php
@@ -3,12 +3,14 @@
 // Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36Failed to connect to localhost port 3000: Connection refused
 // echo $_SERVER["HTTP_USER_AGENT"]; Tarayıcı bilgimi service yazıyor.Bot kullanırken bu şekil bulunuyor.
 
-$oturum = curl_init("http://localhost:3000/contact"); // sadece deneme icin bir olacalde calıştırıyorum.
+$oturum = curl_init("http://localhost/php-dersleri/php_Udmey/curlSistem/islemler.php?islem=normal"); // sadece deneme icin bir olacalde calıştırıyorum.
 
 // $option = array(
 //     CURLOPT_USERAGENT => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36Failed to connect to localhost port 3000: Connection refused",
 //     CURLOPT_REFERER => "http://www.google.com",
 //     CURLOPT_RETURNTRANSFER => true
+//     curl_setopt($oturum, CURLOPT_HEADER, true);
+//     curl_setopt($oturum, CURLOPT_TIMEOUT, 10);
 // ); 
 // curl_setopt_array($oturum,$option)   // Array ile nasıl yazılır. 
 
@@ -19,20 +21,35 @@ curl_setopt($oturum, CURLOPT_REFERER, "http://www.google.com"); // Sayfaya atıl
 
 curl_setopt($oturum, CURLOPT_RETURNTRANSFER, true); // Atılan isteyin gelen değeri ekrana yazdırılmadan ceşitli işlemler yaptırılır 
 
+curl_setopt($oturum, CURLOPT_HEADER, false); // Gelen isteyin header icerigine bunun ile authentication işlemleri vb. yapılabilir.
+
+curl_setopt($oturum, CURLOPT_TIMEOUT, 10); // Atılan isteye gelen cevabın zaman aşımını belirler eger cevap belirlenen süre icinde gelmezse oturum kapatılır.
+
+curl_setopt($oturum, CURLOPT_CONNECTTIMEOUT, 10); // Uygulamaya ilk baglanmak icin zaman aşımı belirleme.
+
+curl_setopt($oturum, CURLOPT_SSL_VERIFYPEER, false); // Istek atılan sitenin SSL sertifika dogrulama mekanizması varsa bot icin bu bilgiyi false yapıyoruz.
+
+
 
 $gelenveri = curl_exec($oturum); // Atılan isteye gelen degeri ekrana yazdırır.degişkene atanması CURLOPT_RETURNTRANSFER true iken velen verilere işlem yapmamızı saglar
 $sonuc = curl_getinfo($oturum); // oturum ile 
 
-// echo "<pre>";
-// print_r(curl_getinfo($oturum));
-// echo "<pre>";
+echo "<pre>";
+// print_r(curl_getinfo($oturum)); //
 
-if ($sonuc["http_code"] != 200) :
-    // echo curl_errno($oturum); hata kodunun numarası
-    echo curl_error($oturum); // hata kodunun stringi
+// if ($sonuc["http_code"] != 200) :
+//     echo curl_errno($oturum); hata kodunun numarası
+//     echo curl_error($oturum); hata kodunun stringi
 
-endif;
+// endif;
 
-echo $gelenveri; // Atılan isteye gelen degeri ekrana yazdırır. degişkene atanması CURLOPT_RETURNTRANSFER true iken velen verilere işlem yapmamızı saglar
+//echo $gelenveri; // Atılan isteye gelen degeri ekrana yazdırır. degişkene atanması CURLOPT_RETURNTRANSFER true iken velen verilere işlem yapmamızı saglar
 
 curl_close($oturum);
+
+// $coz = json_decode($gelenveri, true);
+$coz = json_decode($gelenveri, false); //Nesne olarak erişim
+
+echo $coz->soyadi;
+
+// echo $coz["ad"];
